597a57e2e7289075eca6a303bedc79c574fdefd0
Mikhail Romanenko <705646@gmail.com>
2026-01-29 17:50:15 -0800

    temp resolved warnings

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { useEffect, useState } from "react";
-import type { Schema } from "../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
+import { useState } from "react";
+// import type { Schema } from "../amplify/data/resource";
+// import { generateClient } from "aws-amplify/data";
 import { useAuthenticator } from "@aws-amplify/ui-react";
 
-const client = generateClient<Schema>();
+// const client = generateClient<Schema>();
 
 function App() {
     const [file, setFile] = useState<File | null>(null);
