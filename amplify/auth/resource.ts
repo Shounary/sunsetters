fa14b7f7ff6a7a1c30115b6ccf719ff505bbdbad
Mikhail Romanenko <705646@gmail.com>
2026-02-17 18:14:23 -0800

    readding auth

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -5,17 +5,17 @@ import { postConfirmation } from './post-confirmation/resource';
  * Define and configure your auth resource
  * @see https://docs.amplify.aws/gen2/build-a-backend/auth
  */
-// export const auth = defineAuth({
-//   userAttributes: {
-//     fullname: {
-//       mutable: true,
-//       required: true
-//     }
-//   },
-//   loginWith: {
-//     email: true,
-//   },
-//   triggers: {
-//     postConfirmation
-//   }
-// });
+export const auth = defineAuth({
+  userAttributes: {
+    fullname: {
+      mutable: true,
+      required: true
+    }
+  },
+  loginWith: {
+    email: true,
+  },
+  triggers: {
+    postConfirmation
+  }
+});
