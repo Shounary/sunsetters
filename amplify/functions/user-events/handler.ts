288f30767b3cc653b14fa8f277548a92055b0b71
Mikhail Romanenko <705646@gmail.com>
2026-02-18 16:39:14 -0800

    remove auth refs 2

--- amplify/functions/user-events/handler.ts
+++ amplify/functions/user-events/handler.ts
@@ -1,72 +1,72 @@
-import type { AppSyncResolverHandler } from 'aws-lambda';
-import { SNSClient, PublishCommand } from "@aws-sdk/client-sns";
-import { generateClient } from "aws-amplify/data";
-import { getAmplifyDataClientConfig } from '@aws-amplify/backend/function/runtime';
-import { env } from '$amplify/env/user-events';
-import { Amplify } from 'aws-amplify';
-import type { Schema } from "../../data/resource";
-import { SchemaMutationArgs, UserEvent, UserEventPayload } from '../common/types';
+// import type { AppSyncResolverHandler } from 'aws-lambda';
+// import { SNSClient, PublishCommand } from "@aws-sdk/client-sns";
+// import { generateClient } from "aws-amplify/data";
+// import { getAmplifyDataClientConfig } from '@aws-amplify/backend/function/runtime';
+// import { env } from '$amplify/env/user-events';
+// import { Amplify } from 'aws-amplify';
+// import type { Schema } from "../../data/resource";
+// import { SchemaMutationArgs, UserEvent, UserEventPayload } from '../common/types';
 
 
-const { resourceConfig, libraryOptions } = await getAmplifyDataClientConfig(env);
-Amplify.configure(resourceConfig, libraryOptions);
+// const { resourceConfig, libraryOptions } = await getAmplifyDataClientConfig(env);
+// Amplify.configure(resourceConfig, libraryOptions);
 
-const sns = new SNSClient({});
-const client = generateClient<Schema>();
+// const sns = new SNSClient({});
+// const client = generateClient<Schema>();
 
-export const handler: AppSyncResolverHandler<SchemaMutationArgs, boolean> = async (event) => {
-  const { originUserID, targetUserID, newPostID } = event.arguments
-  const userEvent = event.arguments.userEvent as UserEvent
+// export const handler: AppSyncResolverHandler<SchemaMutationArgs, boolean> = async (event) => {
+//   const { originUserID, targetUserID, newPostID } = event.arguments
+//   const userEvent = event.arguments.userEvent as UserEvent
 
-  console.log(`Received operation: ${userEvent} from user: ${originUserID ?? "undefined"}`);
+//   console.log(`Received operation: ${userEvent} from user: ${originUserID ?? "undefined"}`);
 
-  try {
-    switch (userEvent) {
-      case UserEvent.FOLLOW_USER:
-        if (!originUserID || !targetUserID) throw new Error("Missing originUserID, targetUserID");
-        return await followHandler(originUserID, targetUserID);
+//   try {
+//     switch (userEvent) {
+//       case UserEvent.FOLLOW_USER:
+//         if (!originUserID || !targetUserID) throw new Error("Missing originUserID, targetUserID");
+//         return await followHandler(originUserID, targetUserID);
 
-      case UserEvent.ADD_POST_TO_FEED:
-        if (!newPostID || !originUserID) throw new Error("Missing newPostID, originUserID");
-        return await updateFeedHandler(originUserID, newPostID);
+//       case UserEvent.ADD_POST_TO_FEED:
+//         if (!newPostID || !originUserID) throw new Error("Missing newPostID, originUserID");
+//         return await updateFeedHandler(originUserID, newPostID);
 
-      default:
-        throw new Error(`Unknown operation: ${userEvent}`);
-    }
-  } catch (error) {
-    console.error("Handler failed: ", error);
-    throw error; // Propagate error
-  }
-};
+//       default:
+//         throw new Error(`Unknown operation: ${userEvent}`);
+//     }
+//   } catch (error) {
+//     console.error("Handler failed: ", error);
+//     throw error; // Propagate error
+//   }
+// };
 
-// Individual Handlers
+// // Individual Handlers
 
-async function followHandler(originUserID: string, targetUserID: string) {
-  const origUser = await (await client.models.UserProfile.get({ id: originUserID })).data
-  if (!origUser) {
-    throw Error(`Cannot fect user ${originUserID}`)
-  }
+// async function followHandler(originUserID: string, targetUserID: string) {
+//   const origUser = await (await client.models.UserProfile.get({ id: originUserID })).data
+//   if (!origUser) {
+//     throw Error(`Cannot fect user ${originUserID}`)
+//   }
 
-  client.models.UserProfile.update({
-    id: originUserID,
-    follows: [...origUser.follows ?? [], targetUserID],
-  })
-  await publishToSNS({ userEvent: UserEvent.FOLLOW_USER, originUserID: originUserID, targetUserID: targetUserID });
-  return true;
-}
+//   client.models.UserProfile.update({
+//     id: originUserID,
+//     follows: [...origUser.follows ?? [], targetUserID],
+//   })
+//   await publishToSNS({ userEvent: UserEvent.FOLLOW_USER, originUserID: originUserID, targetUserID: targetUserID });
+//   return true;
+// }
 
-async function updateFeedHandler(originUserID: string, newPostID: string) {
-  await publishToSNS({ userEvent: UserEvent.ADD_POST_TO_FEED, originUserID: originUserID, newPostID: newPostID });
-  return true;
-}
+// async function updateFeedHandler(originUserID: string, newPostID: string) {
+//   await publishToSNS({ userEvent: UserEvent.ADD_POST_TO_FEED, originUserID: originUserID, newPostID: newPostID });
+//   return true;
+// }
 
-async function publishToSNS(payload: UserEventPayload) {
-  await sns.send(new PublishCommand({
-    TopicArn: process.env.SNS_TOPIC_ARN,
-    Message: JSON.stringify({
-      ...payload,
-      timestamp: new Date().toISOString(),
-    })
-  }));
-  console.log(`Published ${payload.userEvent} to SNS`);
-}
+// async function publishToSNS(payload: UserEventPayload) {
+//   await sns.send(new PublishCommand({
+//     TopicArn: process.env.SNS_TOPIC_ARN,
+//     Message: JSON.stringify({
+//       ...payload,
+//       timestamp: new Date().toISOString(),
+//     })
+//   }));
+//   console.log(`Published ${payload.userEvent} to SNS`);
+// }
